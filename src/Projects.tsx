d4a5bcfaa6a3a5abc1c5e2a0abf15a84760171c8
Charles Chitty <charlescchitty@gmail.com>
2024-04-08 15:47:51 -0700

    Updated projects component

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Projects.css";
-import joblyGif from "../public/joblyGif.gif";
-import frienderGif from "../public/frienderGif.gif";
+// import joblyGif from "../public/joblyGif.gif";
+// import frienderGif from "../public/frienderGif.gif";
 
 /** Projects: presentational component for projects
  *
@@ -11,7 +11,7 @@ import frienderGif from "../public/frienderGif.gif";
  * App => RouteList => Projects
  */
 
-function Projects(); JSX.Element {
+function Projects(): JSX.Element {
     return (
         <div>
             <div className="Projects-container">
@@ -30,11 +30,35 @@ function Projects(); JSX.Element {
                     target="_blank"
                     rel="noopener"
                 >
-                    <img src={joblyGif} className="Projects-gif" />
+                    {/* <img src={joblyGif} className="Projects-gif" /> */}
                 </a>
                 <p> Job board where users can sign up, browse companies,and apply to jobs.
                     Companies can post new jobs. Protects routes on unauthorized users. Manages React state.
                     Backenend authentication using jsonschema and JWT. Additional tech: React, React Router, Fetch API, Node.js, Express, PostgreSQL, ElephantSQL, render.&nbsp;
+                </p>
+                <a href="https://friender-demo-app.onrender.com"
+                    target="_blank"
+                    rel="noopener"
+                    className="Projects-project">
+                    <h2 className="Project-title">
+                        Friender
+                    </h2>
+                </a>
+                <a href="https://friender-demo-app.onrender.com"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    {/* <img src={frienderGif} className="Projects-gif" /> */}
+                </a>
+                <p> Social Media Site that lets users upload profile pictures, view other profiles, and links members to local users
+                    Uses AWS S3 to store and retrieve image files. Additional tech: Python, Flask, Jinja, SQLAlchemy, JS.&nbsp;
+                    <a
+                        href="https://github.com/charleschitty/friender"
+                        target="_blank"
+                        rel="noopener"
+                    >
+                        GitHub.
+                    </a>
                 </p>
             </div>
         </div>
