f07804388ba42e000b4013614fd2f9630f9920ad
Charles Chitty <charlescchitty@gmail.com>
2024-04-08 13:02:43 -0700

    Added Navbar component

---
+++ src/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+/** Navbar: component with links to different pages of website
+ *
+ * Props: none
+ * State: none
+ *
+ * App -> Navbar
+ */
+
+function Navbar(): JSX.Element {
+
+    return (
+        <div className="Navbar">
+            <NavLink to="/" end>
+                me
+            </NavLink>
+            <ul>
+                <li>
+                    <NavLink to="/contact">
+                        Contact
+                    </NavLink>
+                </li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default Navbar;
