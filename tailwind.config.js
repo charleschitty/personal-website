93fc6a6e945bd43686322cf4e0b588cfe6f88fbc
Charles Chitty <charlescchitty@gmail.com>
2024-04-15 14:47:04 -0700

    Updated font to include Geist-Mono and Geist

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        geist: ['GeistSans', 'sans'],
+        'geist-mono': ['GeistMono', 'monospace'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
