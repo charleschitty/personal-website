26e163fac9ec44189c193c46aaa2f3e88c184b91
Charles Chitty <charlescchitty@gmail.com>
2024-04-16 05:07:33 -0700

    Added checkbox component

---
+++ src/components/icons/SquareCheck.tsx
@@ -0,0 +1,16 @@
+export const SquareCheckedIcon = (props: React.SVGProps<SVGSVGElement>) => {
+    return (
+        <svg
+            width="11"
+            height="9"
+            viewBox="0 0 11 9"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path
+                d="M1 3.51613L4.30612 7L10 1"
+                stroke="black"
+                stroke-width="1.5">
+            </path>
+        </svg>
+    );
+  };
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,6 @@
 import { GitHubIcon } from "./GitHubIcon";
 import { LinkedInIcon } from "./LinkedInIcon";
 import { CVIcon } from "./CVIcon";
+import { SquareCheckedIcon } from "./SquareCheck";
 
-export { GitHubIcon, LinkedInIcon, CVIcon };
+export { GitHubIcon, LinkedInIcon, CVIcon, SquareCheckedIcon };
